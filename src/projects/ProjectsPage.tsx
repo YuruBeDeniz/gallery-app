41e0b1e4480f21d909ebe33e387138abab299927
Deniz Saganak <saganak.deniz@gmail.com>
2023-01-26 14:57:22 +0100

    create backend REST API

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,33 @@
-import { Fragment, useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import { Fragment, useState, useEffect } from "react";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
+import { projectAPI } from "./projectAPI";
 
 export default function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(1);
+        setError('');
+        setProjects(data);
+      }
+      catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } 
+      finally {
+        setLoading(false);
+      }
+    };
+    loadProjects();
+  }, [])
 
   const saveProject = (project: Project) => {
     //console.log('Saving project: ', project);
@@ -17,7 +40,27 @@ export default function ProjectsPage() {
   return (
     <Fragment>
     <h1>PROJECTS</h1>
+    {error && (
+      <div className="row">
+        <div className="card large error">
+          <section>
+            <p>
+              <span className="icon-alert inverse "></span>
+              {error}
+            </p>
+          </section>
+        </div>
+      </div>
+    )}
+
     <ProjectList projects={projects} onSave={saveProject} />
+
+    {loading && 
+    <div className="center-page">
+      <span className="spinner primary" />
+      <p>Loading...</p>
+    </div>
+    }
     </Fragment>
   )
 }
